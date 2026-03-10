d1153d1b5f9abe0dac56388b10ad8e01a123225e
theubaidistan <ubaidistan395@gmail.com>
2026-03-10 12:17:23 +0500

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,39 @@
+// const data = [
+//   { id: 1, name: "Portfolio" },
+//   { id: 2, name: "Contact" },
+//   { id: 3, name: "Projects" },
+// ];
+
+import dayjs from "dayjs";
+import { navIcons, navLinks } from "../constants";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="font-bold">Muhammad Ubaid Portfolio</p>
+
+        <ul>
+          {navLinks.map(({ id, name }) => (
+            <li key={id}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} className="icon-hover" alt={`icon-${id}`} />
+            </li>
+          ))}
+        </ul>
+        <time>{dayjs().format("ddd MMM D h:mm A")}</time>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
